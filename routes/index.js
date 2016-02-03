dcb2a56c37c550c217b478a3a02257c190ec687f
Optimized4U <support@optimized4u.org>
2016-02-03 19:43:16 +0000

    Set up pages and nav.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Real Racing Database' });
+});
+
+router.get('/records', function(req, res, next) {
+  res.render('records', { title: 'Records' });
+});
+
+router.get('/reports', function(req, res, next) {
+  res.render('reports', { title: 'Reports' });
 });
 
 module.exports = router;
