0522758160faccb699e259c4d527d132070ccfc7
Optimized4U <support@optimized4u.org>
2016-02-20 18:00:13 +0000

    Added active nav list class switching.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Real Racing Database' });
+  res.render('index', { title: 'Home' });
 });
 
 router.get('/records', function(req, res, next) {
